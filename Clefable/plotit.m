b2e732c14ad3b0ae0f2f729a0a8013a67f4c85ea
wlsdx <38873154+wlsdx@users.noreply.github.com>
2019-07-30 23:56:40 +0800

    Update and rename plot.m to plotit.m

--- Clefable/plotit.m
+++ Clefable/plotit.m
@@ -2,4 +2,4 @@ C=load('Clef vs Celesteela.txt');
 n=linspace(1,length(C),length(C))';
 rate=C./n;
 plot(n,rate);
-C(end)%×ÜÊ¤³¡
+disp(['Celesteela wins ',int2str(C(end)),' in ',int2str(length(C)),' games']);
